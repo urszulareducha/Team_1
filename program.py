29e6258064149a6901c97c7008d674535d4a9ed1
juwilke <julia.justyna@gmail.com>
2023-01-22 15:19:31 +0100

    rename variable as request

--- program.py
+++ program.py
@@ -1,20 +1,39 @@
-wiek = input("Podaj wiek użytkownika jako liczbę całkowitą:")
+#Program dla dorosłych do alkoholu
+wiek = input("Podaj wiek użytkownika jako liczbe calkowitą:")
 # Sprawdzamy czy podany wiek jest liczbą
 if wiek.isdigit() == False:
-    exit("Wiek musi być liczbą albo podana liczba nie jest całkowitąa")
-wiek = int(wiek)
+	exit("Wiek musi być liczbą albo podana liczba nie jest calkowita")
+wiek=int(wiek)
 if wiek>=18 and wiek<=40:
-    print("Witaj w naszej apce z alkoholem, zapraszamy do zakupów")
+	print("Witaj w naszej apce z alkoholem, zapraszamy do zakupów")
 elif wiek>40:
-    print("Witaj w naszej apce z alkoholem, zapraszamy do zakupów")
-    print("Uważaj w Twoim wieku nie przasadzaj ze spożyciem")
+	print("Witaj w naszej apce z alkoholem, zapraszamy do zakupów")
+	print("Uważaj w Twoim wieku nie przasadzaj ze spożyciem")
 else:
   exit("Jesteś za młoda/y na alkohol. Zapraszamy na disney.com")
 
-gender = str()
-gender = input("podaj płeć ")
+region=input("Skad pochodzisz (Europa/USA):")
 
-if gender == "K" and wiek>30:
+region=str(region)
+if region=="Europa":
+    print("Cześć! Hola! Ciao!")
+elif region=="USA":
+    print("Hello!")
+else:
+    print("Wybierz region")
+    region=input("Skad pochodzisz (Europa/USA):")
+
+plec = input("Jesteś kobietą czy mężczyzną? Wpisz K / M")
+if plec == "K":
+    print("Witaj")
+elif plec == "M":
+    print("Witaj")
+else:
+    print("Nie rozpoznałem płci. Podaj proszę czy jesteś K czy M?")
+plec = str()
+plec = input("podaj płeć ")
+
+if plec == "K" and wiek>30:
     print("Hej, odbierz swój pierwszy aperol spritz za darmo")
 else:
 	exit()
