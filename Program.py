f73b203bdb0d11dc51dbdfbfd60b2be4ecd088bb
JustSek <ju.sekala@gmail.com>
2023-01-22 13:48:55 +0100

    Obsługa wieku >120

--- Program.py
+++ Program.py
@@ -4,6 +4,8 @@ wiek = input("Podaj wiek użytkownika jako liczbe calkowitą:")
 if wiek.isdigit() == False:
 	exit("Wiek musi być liczbą albo podana liczba nie jest calkowita")
 wiek=int(wiek)
+if wiek>=120
+    print("Nikt tyle nie żyje, nie kłam z wiekiem")
 if wiek>=18 and wiek<=40:
 	print("Witaj w naszej apce z alkoholem, zapraszamy do zakupów")
 elif wiek>40:
